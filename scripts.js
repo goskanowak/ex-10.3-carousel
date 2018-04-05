99b88664aec9315f4da3580df02b1154580f0535
Małgorzata Nowak <goskanowak87@gmail.com>
2018-04-05 12:14:11 +0200

    Update files

---
+++ scripts.js
@@ -0,0 +1,15 @@
+$(function(){
+    var carouselList = $('#carousel ul');
+    
+    setInterval(function(changeSlide){
+     carouselList.animate({'marginLeft':-400}, 15000, 'linear', function(moveFirstSlide){
+        var firstItem = carouselList.find('li:first'),
+        lastItem = carouselList.find('li:last');
+        lastItem.after(firstItem);
+        carouselList.css({marginLeft:0});
+    });
+ 
+       });
+    
+});
+ 
