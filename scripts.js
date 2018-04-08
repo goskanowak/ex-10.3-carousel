367e4d2011805eb0bb00cc111586d548bf4540a0
Małgorzata Nowak <goskanowak87@gmail.com>
2018-04-09 00:46:31 +0200

    Change function

--- scripts.js
+++ scripts.js
@@ -2,14 +2,12 @@ $(function(){
     var carouselList = $('#carousel ul');
     
     setInterval(function(changeSlide){
-     carouselList.animate({'marginLeft':-400}, 15000, 'linear', function(moveFirstSlide){
-        var firstItem = carouselList.find('li:first'),
-        lastItem = carouselList.find('li:last');
-        lastItem.after(firstItem);
-        carouselList.css({marginLeft:0});
-    });
- 
+        carouselList.animate({'marginLeft':-400}, 15000, 'linear', function(moveFirstSlide){
+            var firstItem = carouselList.find('li:first'),
+            lastItem = carouselList.find('li:last');
+            lastItem.after(firstItem);
+            carouselList.css({marginLeft:0});
+            });
        });
-    
 });
  
